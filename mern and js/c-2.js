8f6e53902627d79596dc08c490b4a94db61c0fb6
Prabhas200 <your-email@example.com>
2026-08-05 11:54:04 -0500

    C

--- mern and js/c-2.js
+++ mern and js/c-2.js
@@ -1,3 +1,21 @@
 
 // chapter 2: operators and conditional statements
 
+// operators: +, -, *, /, %, ++, --, ==, ===, !=, !==, >, <, >=, <=, &&, ||, !, ?:
+
+// arithmetic operators: +, -, *, /, % are used to perform arithmetic operations on numbers
+
+// increment and decrement operators: ++ and -- are unary operators that increase or decrease the value of a variable by 1
+
+// comparison operators: ==, ===, !=, !==, >, <, >=, <= are binary operators that compare two values and return true or false
+
+// conditional statements: if, else, else if and switch are used to control the flow of a program based on a condition or a set of conditions 
+
+// logical operators: &&, ||, ! are used to combine conditions and return true or false 
+
+// assignment operators: =, +=, -=, *=, /= and %= are used to assign values to variables 
+
+// ternary operator: ? is used to assign a value to a variable based on a condition
+
+// null coalescing operator: ?? is used to assign a value to a variable if it is null or undefined
+
