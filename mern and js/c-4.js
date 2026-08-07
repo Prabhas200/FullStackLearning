9d49613b257fe483a16773bbd88751c708386b3e
Prabhas200 <your-email@example.com>
2026-08-06 21:46:46 -0500

    c-4

--- mern and js/c-4.js
+++ mern and js/c-4.js
@@ -1,3 +1,23 @@
+// chapter 4: arrays and its methods
+
+// arrays: arrays are a collection of elements of the same type stored in contiguous memory locations.
+let marks = [90, 80, 70, 60, 50];
+console.log(marks);
+
+let names = ['john', 'jane', 'joe'];
+console.log(names);
+
+console.log(names.length);
+
+
+// array indices: 
+// the index of an array is the position of the element in the array.
+// the first element in an array is at index 0. the last element in an array is at index length - 1.
+// the length property is a read-only property. it returns the number of elements in the array.
+console.log(names[0]);
+console.names[3]; // index out of bound error shows output as undefined
+console.log(marks[2]=23); // to change the value of an array element at a particular index we use array[index]=value in console which mean it can be changed because its a mutable and we cant change the length of an array in strict mode because they are immutable
+
 
 // looping over arrays: this prints all the elements of the array in the console.
 // loops are used to execute a block of code multiple times or until a certain condition is met and its an iterable statement that consist of three parts: initialization, condition and increment or strings, objects, arrays and functions.
