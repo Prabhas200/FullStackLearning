c11ec8b77545739b5881eeb9139d9d5a85f22698
Prabhas200 <your-email@example.com>
2026-08-18 18:34:41 -0500

    update in c-6

---
+++ mern and js/c-6/c6.js
@@ -0,0 +1,33 @@
+// chapter 6: objects and its methods {DOM Manipulation and events in js} document object model
+
+// objects: objects are collections of related data and behavior.
+// objects are used to store data and to manipulate it.
+
+//? DOM: the document object model (DOM) is a programming interface for HTML and XML documents. It represents the structure and content of a document as a tree of objects.
+
+//? events: events are actions that occur in a web page, such as a mouse click, a key press, or a form submission.
+
+//? event handlers: event handlers are functions that are called when an event occurs in a web page.
+
+//? event listeners: event listeners are objects that are used to listen for and respond to events in a web page.
+
+//? event propagation: event propagation is the process of how events are handled in a web page.
+
+//? event bubbling: event bubbling is the process of how events are handled in a web page.
+
+//? event capturing: event capturing is the process of how events are handled in a web page.
+
+//? event delegation: event delegation is the process of how events are handled in a web page.
+
+
+
+//! DOM manipulation and events in js part 1
+
+
+
+// There are 3 types of musketeers: html(structure), css(style) and js(behavior or logic).
+
+// <style type="text/css"> this is css tag that is used to style the html tag</style>
+// <script type="text/javascript"> this is js tag that is used to add logic to the html tag</script>
+
+
--- mern and js/practice.js
+++ mern and js/practice.js
@@ -8,5 +8,15 @@
 
 
 
+// chapter 6: objects and its methods {DOM Manipulation and events in js} document object model
+
+// DOM manipulation and events in js part 1
+
+
+
+// There are 3 types of musketeers: html(structure), css(style) and js(behavior or logic).
+
+// <style type="text/css"> this is css tag that is used to style the html tag</style>
+// <script type="text/javascript"> this is js tag that is used to add logic to the html tag</script>
 
 
